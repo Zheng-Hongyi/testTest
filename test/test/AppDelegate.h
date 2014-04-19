e4e405730a6ee8892d36ff8800c6386f01f1620b
zhenghongyi <xiaodaier@yeah.net>
2014-04-19 11:14:32 +0800

    test

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Hongyi Zheng on 4/19/14.
+//  Copyright (c) 2014 Hongyi Zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
